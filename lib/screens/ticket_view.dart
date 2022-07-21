a61abd28dadb70b56a51640977b97f06c6007be6
AsaduzzamanSam <samrat.asad@gmail.com>
2022-07-21 08:25:44 +0600

    ticket orange small part done

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,6 +1,7 @@
 import 'package:air_ticket_flutter/utils/app_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:air_ticket_flutter/utils/app_style.dart';
+import 'package:gap/gap.dart';
 
 import '../widgets/thick_container.dart';
 
@@ -14,15 +15,18 @@ class TicketView extends StatelessWidget {
       width: size.width,
       height: 200,
       child: Container(
-        margin: EdgeInsets.only(
+        margin: const EdgeInsets.only(
           left: 16,
         ),
         child: Column(
           children: [
+            /*
+            Blue ticket part
+             */
             Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFF526799),
-                borderRadius: const BorderRadius.only(
+              decoration: const BoxDecoration(
+                color: Color(0xFF526799),
+                borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(21),
                   topRight: Radius.circular(21),
                 ),
@@ -38,7 +42,7 @@ class TicketView extends StatelessWidget {
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
                       const Spacer(),
-                      ThickContainer(),
+                      const ThickContainer(),
                       Expanded(
                         child: Stack(
                           children: [
@@ -72,7 +76,7 @@ class TicketView extends StatelessWidget {
                             Center(
                               child: Transform.rotate(
                                 angle: 1.5,
-                                child: Icon(
+                                child: const Icon(
                                   Icons.local_airport_rounded,
                                   color: Colors.white,
                                 ),
@@ -81,14 +85,104 @@ class TicketView extends StatelessWidget {
                           ],
                         ),
                       ),
-                      ThickContainer(),
+                      const ThickContainer(),
                       const Spacer(),
                       Text(
-                        'London',
+                        'LDN',
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       )
                     ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'New-York',
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                      Text(
+                        '8H 30M',
+                        style:
+                            Styles.headLineStyle4.copyWith(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'London',
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            /*
+            Orange ticket part
+             */
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          return Flex(
+                            direction: Axis.horizontal,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            children: List.generate(
+                              (constraints.constrainWidth() / 15).floor(),
+                              (index) => const SizedBox(
+                                width: 5,
+                                height: 1,
+                                child: DecoratedBox(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ),
