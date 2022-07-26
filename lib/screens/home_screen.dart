ff6d62498f149939fcb38aeaa28ada641758dc2c
its-asaduzzaman <samrat.asad@gmail.com>
2022-07-26 23:37:41 +0600

    added ticket list data and show hotel and ticket list dynamic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:air_ticket_flutter/screens/hotel_screen.dart';
 import 'package:air_ticket_flutter/screens/ticket_view.dart';
+import 'package:air_ticket_flutter/utils/app_info_list.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:air_ticket_flutter/utils/app_style.dart';
@@ -93,10 +94,9 @@ class HomeScreen extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.only(left: 10),
             child: Row(
-              children: [
-                TicketView(),
-                TicketView(),
-              ],
+              children: ticketList
+                  .map((singleTicket) => TicketView(ticket: singleTicket))
+                  .toList(),
             ),
           ),
           const Gap(15),
@@ -126,12 +126,9 @@ class HomeScreen extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.only(left: 20),
             child: Row(
-              children: [
-                HotelScreen(),
-                HotelScreen(),
-                HotelScreen(),
-                HotelScreen(),
-              ],
+              children: hotelList
+                  .map((singleHotel) => HotelScreen(hotel: singleHotel))
+                  .toList(),
             ),
           )
         ],
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -6,7 +6,8 @@ import 'package:gap/gap.dart';
 import '../widgets/thick_container.dart';
 
 class TicketView extends StatelessWidget {
-  const TicketView({Key? key}) : super(key: key);
+  const TicketView({Key? key, required this.ticket}) : super(key: key);
+  final Map<String, dynamic> ticket;
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +38,7 @@ class TicketView extends StatelessWidget {
                   Row(
                     children: [
                       Text(
-                        'NYC',
+                        ticket['from']['code'],
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
@@ -88,7 +89,7 @@ class TicketView extends StatelessWidget {
                       const ThickContainer(),
                       const Spacer(),
                       Text(
-                        'LDN',
+                        ticket['to']['code'],
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       )
@@ -101,20 +102,20 @@ class TicketView extends StatelessWidget {
                       SizedBox(
                         width: 100,
                         child: Text(
-                          'New-York',
+                          ticket['from']['name'],
                           style: Styles.headLineStyle4
                               .copyWith(color: Colors.white),
                         ),
                       ),
                       Text(
-                        '8H 30M',
+                        ticket['flying_time'],
                         style:
                             Styles.headLineStyle4.copyWith(color: Colors.white),
                       ),
                       SizedBox(
                         width: 100,
                         child: Text(
-                          'London',
+                          ticket['to']['name'],
                           style: Styles.headLineStyle4
                               .copyWith(color: Colors.white),
                           textAlign: TextAlign.end,
@@ -203,19 +204,19 @@ class TicketView extends StatelessWidget {
                   Row(
                     children: [
                       Text(
-                        '1 May',
+                        ticket['date'],
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
                       const Spacer(),
                       Text(
-                        '08:00 AM',
+                        ticket['departure_time'],
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
                       const Spacer(),
                       Text(
-                        '23',
+                        ticket['number'].toString(),
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       )
